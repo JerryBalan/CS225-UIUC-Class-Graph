de33f36889438a8b4017381f863fcd511f8284b9
Jerry Balan <agbalan2@illinois.edu>
2020-12-07 11:32:54 -0600

    updated djikstras

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,4 +1,9 @@
 #include "classes.h"
+#include <unordered_map>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <functional>
 using std::string;
 using std::vector;
 
@@ -38,6 +43,42 @@ vector<string> Classes::bfs(string origin, unsigned depth) {
 
 vector<string> Classes::shortestPath(string origin, string dest) {
   vector<string> path;
+    //std::unordered_map<string, int> path;
+  //vector<int> dists;
+  std::unordered_map<string, int> dists;
+  int inf = INT_MAX;
+  for(Vertex v : g_.getVertices()) {
+    path.push_back(nullptr);
+    //path[v] = nullptr;
+    dists[v] = inf;
+  }
+  dists[origin] = 0;
+
+  std::priority_queue<std::pair<int, string>, vector<std::pair<int, string>>, std::greater<std::pair<int, string>>> q; // pq of verticies, minheap syntax
+  //build pq
+  q.push(make_pair(0, origin));
+
+  while(!q.empty()) {
+    string vtxClass = q.top().second;
+    path.push_back(vtxClass);
+    q.pop();
+    vector<string> adjVtxs = g_.getAdjacent(vtxClass);
+    for(Vertex v : adjVtxs) {
+      if(dists[v] == inf) {
+        q.push(make_pair(dists[v], v));
+        dists[v] = dists[vtxClass] + 1;
+      }
+      /*
+      if(std::find(path.begin(), path.end(), v) == path.end()) { // v is not in path
+        if(dists[vtxClass] + 1 < dists[v]) { // 1 represents each edge weight, classes equivalent so 1
+          q.push(make_pair());
+          dists[v] = dists[vtxClass] + 1;
+          path[v] // fix
+        }
+      }
+      */
+    }
+  }
 
   return path;
 }
