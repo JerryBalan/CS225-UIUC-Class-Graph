b891319330eac54520f6a55fc1e1d98ea269c947
Hassan F <1hassanfarooq@gmail.com>
2020-12-10 22:31:22 -0600

    Updated tests

--- src/tests/test_part1.cpp
+++ src/tests/test_part1.cpp
@@ -16,7 +16,7 @@ TEST_CASE("BFS-CS225", "[weight=10][part1]")
 {
 	Classes test;
 	Graph g = test.getGraph();
-	std::vector<Vertex> output, expectedOutput{"CS 225", "CS 125", "ECE 220", "ECE 120", "CS 173", "MATH 213"};
+	std::vector<Vertex> output, expectedOutput{"CS 125", "CS 173", "CS 225", "ECE 120", "ECE 220", "MATH 112", "MATH 213", "MATH 220", "MATH 221"};
 	g.BFS("CS 225", output);
 
 	std::sort(output.begin(), output.end());
@@ -38,7 +38,7 @@ TEST_CASE("BFS-ECE391", "[weight=10][part1]")
 {
 	Classes test;
 	Graph g = test.getGraph();
-	std::vector<Vertex> output, expectedOutput{"ECE 391", "ECE 220", "CS 233", "ECE 120", "CS 125", "CS 173", "CS 225"};
+	std::vector<Vertex> output, expectedOutput{"CS 125", "CS 173", "CS 225", "CS 233", "ECE 120", "ECE 220", "ECE 391", "MATH 112", "MATH 213", "MATH 220", "MATH 221"};
 	g.BFS("ECE 391", output);
 
 	std::sort(output.begin(), output.end());
@@ -52,7 +52,7 @@ TEST_CASE("BFS-ECE420", "[weight=10][part1]")
 {
 	Classes test;
 	Graph g = test.getGraph();
-	std::vector<Vertex> output, expectedOutput{"ECE 420", "ECE 310", "ECE 210", "ECE 110", "PHYS 212", "MATH 285", "MATH 286"};
+	std::vector<Vertex> output, expectedOutput{"ECE 110", "ECE 210", "ECE 310", "ECE 420", "MATH 220", "MATH 221", "MATH 231", "MATH 241", "MATH 285", "MATH 286", "PHYS 211", "PHYS 212"};
 	g.BFS("ECE 420", output);
 
 	std::sort(output.begin(), output.end());
